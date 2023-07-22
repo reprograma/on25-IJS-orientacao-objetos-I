12bc7e8945c59c506410f3ec1f252f2e125c01d9
hinale <leticiaamarallara@hotmail.com>
2023-07-22 11:49:30 -0300

    adiciona exercício em aula - exercício 3

---
+++ Exercícios/Para sala/Exercício 3/index.js
@@ -0,0 +1,38 @@
+class Account {
+  id;
+  name;
+  balance;
+
+  constructor(id, name, balance) {
+    this.id = id
+    this.name = name
+    this.balance = balance
+  }
+
+  credit(amount) {
+    this.balance = this.balance + amount
+    console.log(`New balance: ${this.balance}`)
+  }
+
+  debit(amount) {
+    this.balance = this.balance - amount
+    console.log(`New balance: ${this.balance}`)
+  }
+
+  transferTo(anotherAccount, amount) {
+    if(this.balance < amount) {
+      console.log("Insufficient balance account")
+    } else {
+      this.debit(amount)
+      anotherAccount.credit(amount)
+      console.log(`New balance for your account: ${this.balance}`)
+      console.log(`New balance for another account: ${anotherAccount.balance}`)
+    }
+  }
+}
+
+const 
+  account1 = new Account(1, "One Person", 200),
+  account2 = new Account(2, "Another Person", 100)
+account1.transferTo(account2, 300) //negative case
+account1.transferTo(account2, 100) //positive case
