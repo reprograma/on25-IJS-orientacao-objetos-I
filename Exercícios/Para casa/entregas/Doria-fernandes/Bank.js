22e4c139c8ed3b88d95f5e6fa7d53d2458e0f772
DoriansFer <doohfernandes@yahoo.com>
2023-07-23 11:35:31 -0300

    Class Bank

---
+++ Exercícios/Para casa/entregas/Doria-fernandes/Bank.js
@@ -0,0 +1,38 @@
+class Bank {
+    bankCode;
+    bankName;
+    #transferTax;
+    static createdBanks = [];
+    constructor(bankCode, bankName, transferTax) {
+        this.#transferTax = transferTax;
+        this.bankCode = bankCode;
+        this.bankName = bankName;
+    }
+
+    get transferTax() {
+        return this.#transferTax;
+    }
+    set transferTax(newTax) {
+        return this.#transferTax = newTax;
+    }
+}
+
+let caixa = new Bank(104, "Caixa", 1.04)
+let bradesco = new Bank(237, "Bradesco", 1.20)
+let itau = new Bank(341, "Itaú", 0.89)
+let pagBank = new Bank(290, "PagBank", 1.00)
+let nuBank = new Bank(260, "NuBank", 0.90)
+let bb = new Bank(1, "Banco do Brasil", 0.67)
+let santander = new Bank(33, "Santander", 1.15)
+
+console.log(caixa) // Bank { bankCode: 104, bankName: 'Caixa'}
+console.log(caixa.transferTax); // 1.04
+caixa.transferTax = 1.14
+console.log(caixa.transferTax); // 1.14
+
+console.log(bradesco) // Bank { bankCode: 237, bankName: 'Bradesco' }
+console.log(itau) // Bank { bankCode: 341, bankName: 'Itaú' }
+console.log(pagBank) // Bank { bankCode: 290, bankName: 'PagBank' }
+console.log(nuBank) // Bank { bankCode: 260, bankName: 'NuBank' }
+console.log(bb) // Bank { bankCode: 1, bankName: 'Banco do Brasil' }
+console.log(santander) // Bank { bankCode: 33, bankName: 'Santander' }
