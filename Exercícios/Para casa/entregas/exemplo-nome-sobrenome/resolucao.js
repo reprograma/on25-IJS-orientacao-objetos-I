21fddace7f1814996638522bb437fedd1f779c76
Ellie <elliekonuma@gmail.com>
2023-07-26 12:23:06 -0300

    update home assigniment

--- Exercícios/Para casa/entregas/exemplo-nome-sobrenome/resolucao.js
+++ Exercícios/Para casa/entregas/exemplo-nome-sobrenome/resolucao.js
@@ -1,7 +1,21 @@
+//// importar classes
 const Bank = require("./Bank");
+
 const Client = require("./Client");
+
 const BankAccount = require("./BankAccount");
 
+///// tests
+const nuBank = new Bank(160, "Nubank", 0);
 console.log(Bank.createdBanks);
-const nuBank = new Bank(123, "Nubank", 0);
 const bradesco = new Bank(222, "Bradesco", 5);
+console.log(nuBank);
+
+const jef = new Client("Jef", 123123123);
+
+console.log(jef);
+
+jefAccount = new BankAccount(jef, nuBank, 3333, 4444);
+console.log(jefAccount);
+
+console.log(Bank.createdBanks);
