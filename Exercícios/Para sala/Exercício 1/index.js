404e6edfd58fefb5e2c1a6e591492629cd122770
michelefeitosa <michelefeitosa08@gmail.com>
2023-07-25 21:09:55 -0300

    anotações da aula

---
+++ Exercícios/Para sala/Exercício 1/index.js
@@ -0,0 +1,20 @@
+class Employee{
+    id;
+    name;
+    salary;
+
+   constructor(id, name, salary){
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+   }
+
+   raiseSalary(percent){
+    this.salary += (this.salary * (percent/100))
+   }
+}
+
+const employee1 = new Employee (123, "michele", 15000)
+console.log(employee1);
+employee1.raiseSalary(10)
+console.log(employee1.salary)
