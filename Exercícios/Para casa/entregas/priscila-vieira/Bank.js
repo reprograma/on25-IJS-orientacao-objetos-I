4e6e63c5678378cd455b6fa32247b5ea3e39b4c5
Priscila Vieira <priscilabg@gmail.com>
2023-07-26 23:17:13 -0300

    feat: Semana 5 | POO

---
+++ Exercícios/Para casa/entregas/priscila-vieira/Bank.js
@@ -0,0 +1,28 @@
+class Bank {
+  bankCode;
+  bankName;
+  #transferTax;
+
+  static createdBanks = []; // bankCode, qtdClients (inicializado com 0) pertence a classe não a obj
+
+  constructor(bankCode, bankName, transferTax){
+    this.bankCode = bankCode;
+    this.bankName = bankName;
+    this.#transferTax = transferTax;
+
+    this.constructor.createdBanks.push({ 
+      bankCode: this.bankCode, 
+      qtdClients: 0
+    });
+  }
+  
+  get transferTax(){
+    return this.#transferTax;
+  }
+
+  set transferTax(newTransferTax){
+    this.#transferTax = newTransferTax;
+  }
+}
+
+module.exports = ({ Bank )};
