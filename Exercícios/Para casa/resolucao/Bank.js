68cd0adec613e8bb68e855329e39900adf31c913
laisbagno <laisbags@gmail.com>
2023-07-26 23:06:14 -0300

    entregando exercicio

--- Exercícios/Para casa/resolucao/Bank.js
+++ Exercícios/Para casa/resolucao/Bank.js
@@ -1,17 +1,31 @@
-class Bank {
-	bankCode;
-	bankName;
+class Bank{
+    bankCode
+    bankName
+    #transferTax
+    numClients
+    static createdBanks = []
 
-	static createdBanks = [];
+    constructor(bankCode, bankName, transferTax){
+        this.bankCode = bankCode
+        this.bankName = bankName
+        this.#transferTax = transferTax
+        this.numClients = 0
+        this.constructor.createdBanks.push({
+            bankCode: this.bankCode, 
+            numClients: this.numClients})
+    }
 
-	constructor(bankCode, bankName) {
-		this.bankCode = bankCode;
-		this.bankName = bankName;
-		this.constructor.createdBanks.push({
-			bankCode: this.bankCode,
-			qtdClients: 0,
-		});
-	}
+    get transferTax() {
+        return this.#transferTax
+    }
+
+    set transferTax(newTransferTax) {
+        this.#transferTax = newTransferTax
+    }
+
+
 }
 
-module.exports = { Bank }
+const bank1 = new Bank(100, "laisBank", 0.1)
+
+module.exports = { Bank, bank1 }
