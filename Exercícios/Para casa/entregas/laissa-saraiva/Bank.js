35fde1fae763e069830d125fe2cfbba3f4da7a17
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-07-26 23:21:16 -0300

    refactor: refatora Class

--- Exercícios/Para casa/entregas/laissa-saraiva/Bank.js
+++ Exercícios/Para casa/entregas/laissa-saraiva/Bank.js
@@ -9,9 +9,7 @@ class Bank {
     this.bankCode = bankCode;
     this.bankName = bankName;
     this.#transferTax = transferTax;
-    this.constructor.createdBanks.push;
-    this.qtdClient = 0;
-    ({bankCode: this.bankCode, qtdClientes: this.qtdClient })
+    this.constructor.createdBanks.push({bankCode: this.bankCode, qtdClients: 0 })
   }
 
   get transferTax() {
@@ -22,14 +20,5 @@ class Bank {
     return this.#transferTax = newTax;
   }
 }
-
-const laissaBank = new Bank( 123 , "laissaBank", 0.05);
-const devBank = new Bank( 456, "DevBank", 0.08) 
-console.log(laissaBank);
-console.log(laissaBank.transferTax)
-laissaBank.transferTax = 0.06;
-console.log(laissaBank.transferTax)
-
-console.log(Bank.createdBanks)
 
 module.exports = {Bank};
